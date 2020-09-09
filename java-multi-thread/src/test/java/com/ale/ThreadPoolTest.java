951502771e3aa476ac223de72bbcf39127e83f69
alewu <wywujiawei@163.com>
2020-09-09 18:04:41 +0800

    update

---
+++ java-multi-thread/src/test/java/com/ale/ThreadPoolTest.java
@@ -0,0 +1,53 @@
+package com.ale;
+
+import org.junit.jupiter.api.Test;
+
+import java.text.MessageFormat;
+import java.util.concurrent.*;
+
+class ThreadPoolTest {
+
+    private static final CountDownLatch count = new CountDownLatch(5);
+
+    ThreadPoolExecutor pool = new ThreadPoolExecutor(
+            1, 2, 10,
+            TimeUnit.SECONDS,
+            new ArrayBlockingQueue<>(5), (ThreadFactory) Thread::new,
+            new ThreadPoolExecutor.AbortPolicy());
+
+    @Test
+    void test() {
+        ExecutorService service = Executors.newFixedThreadPool(2);
+        service.execute(() -> {
+            sleep(10);
+        });
+        service.execute(() -> {
+            sleep(10);
+        });
+    }
+
+    @Test
+    void testThread() throws InterruptedException {
+        pool.execute(() -> sleep(100));
+        pool.execute(() -> sleep(100));
+        pool.execute(() -> sleep(200));
+        pool.execute(() -> sleep(200));
+        pool.execute(() -> sleep(200));
+        pool.execute(() -> sleep(200));
+        pool.execute(() -> sleep(200));
+        count.await();
+
+    }
+
+    private void sleep(int i) {
+        System.out.println(String.format("线程活跃数: %d, 任务数量： %d", pool.getActiveCount(), pool.getQueue().size()));
+        System.out.println(String.format("线程%s 执行 sleep 方法", Thread.currentThread().getName()));
+        try {
+            TimeUnit.SECONDS.sleep(i);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        count.countDown();
+
+    }
+}
